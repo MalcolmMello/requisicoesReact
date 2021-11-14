85ebef21d0ca24c5376f0ca1ce44b1ae6a559cb5
Malcolm Lima <malcolmmellolima@gmail.com>
2021-11-14 11:30:20 -0300

    fazendo requisição e exibindo na tela sem async await

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Requisições com react 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type Cars = {
+    [tag: string]: { 
+      id: number,
+      brand: string,
+      name: string,
+      year: number,
+      photo: string,
+      price: number
+  }
 }
 
-export default App;
+export default () => {
+  const [cars, setCars] = useState<Cars[]>([])
+
+  const getCars  = () => {
+    fetch('https://api.b7web.com.br/carros/api/carros')
+      .then(function(result) {
+        return result.json();
+      })
+      .then(function(json) {
+        if(json.error === '') {
+          setCars( json.cars )
+        } else {
+          alert( json.error )
+        }
+      })
+  }
+
+  useEffect(()=>{
+    getCars()
+  }, [])
+
+  return (
+    <div>
+      <h1>Lista de Carros</h1>
+      <button onClick={getCars}>Atualizar Lista</button>
+
+      <hr />
+
+      {cars.map((item, index)=>(
+        <div key={index}>
+        <img src={`${item.photo}`}  width="200"/>
+          <h3>{item.brand} - {item.name}</h3>
+          <p>{item.year} - R$ {item.price}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
