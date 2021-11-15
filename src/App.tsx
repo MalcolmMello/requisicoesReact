1649f8e1163550b9552d590b2597ddd8635266a5
Malcolm Lima <malcolmmellolima@gmail.com>
2021-11-15 09:07:42 -0300

    requisição method post login

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { SelectHTMLAttributes, useEffect, useState } from "react"
+import React, { FormEventHandler, FormHTMLAttributes, SelectHTMLAttributes, useEffect, useState } from "react"
 
 type Cars = {
     [tag: string]: { 
@@ -16,6 +16,9 @@ export default () => {
   const [cars, setCars] = useState<Cars[]>([])
   const [loading, setLoading] = useState(false)
   const [year, setYear] = useState('')
+
+  const [emailField, setEmailField] = useState('')
+  const [passwordField, setPassword] = useState('')
 
   const getCars  = async () => {
     setLoading(true);
@@ -41,9 +44,50 @@ export default () => {
     setYear(e.target.value)
   }
 
+  const handleloginSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    let url = 'https://api.b7web.com.br/carros/api/auth/login'
+
+
+    let result = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: emailField,
+        password: passwordField
+      })
+    })
+    let json = await result.json()
+
+    if(json.error != '') {
+      alert( json.error )
+    }
+
+    console.log('RESULT', json)
+  }
+
   return (
     <div>
 
+      <h2>Faça Login</h2>
+      <form onSubmit={handleloginSubmit}>
+        <label>
+          E-mail:
+          <input type="email" value={emailField} onChange={e=>setEmailField(e.target.value)}/>
+        </label> <br/>
+
+        <label>
+          Senha:
+          <input type="password" value={passwordField} onChange={e=>setPassword(e.target.value)}/>
+        </label> <br/>
+
+        <input type="submit" value="Enviar"/>
+      </form>
+
+      <hr />
 
       <h1>Lista de Carros</h1>
 
